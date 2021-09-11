3c3c6c8792f7bfb1511399492fedb3860bc09ac0
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2021-09-11 21:20:42 +0530

    Added mvc to application.

---
+++ BookStoreApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content($"Hello this is Index action.");
+        }
+    }
+}
